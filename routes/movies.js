37c727ae9d3aaab1066d684ea6073b98d261b30a
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-08-21 23:47:24 +0200

    exerciseDone

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const Project = require('../models/Project');
 
 // @desc    Get all movies
 // @route   GET /
@@ -7,35 +8,72 @@ router.get('/', async (req, res, next) => {
   // Run 'npm install' and 'npm run dev' and check on Postman if a GET request 
   // to http://localhost:8000/api/v1/movies returns the following response.
   // If it does, you are ready to work!
-  res.status(200).json({ response: 'hello' })
+  try {
+    const projects = await Project.find({});
+    if (projects.length === 0) {
+      res.status(200).json({ response: 'No projects found in the database ' });
+    } else {
+      res.status(200).json({ data: projects })
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // @desc    Get single movie
 // @route   GET /:id
 // @access  Public
 router.get('/:id', async (req, res, next) => {
-
+  const { id } = req.params;
+  try {
+    const project = await Project.findById(id);
+    if (!project) {
+      res.status(404).json({ response: 'Project not found' });
+    } else {
+      res.status(200).json({ data: project })
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // @desc    Create a movie
 // @route   POST /
 // @access  Public
 router.post('/', async (req, res, next) => {
-
+  try {
+    const project = await Project.create(req.body);
+    res.status(201).json({ data: project })
+  } catch (error) {
+    next(error);
+  }
 });
 
 // @desc    Edit a movie
 // @route   PUT /:id
 // @access  Public
 router.put('/:id', async (req, res, next) => {
-
+  const { id } = req.params;
+  const { title, description } = req.body;
+  try {
+    const updatedProject = await Project.findByIdAndUpdate(id, { title, description }, { new: true });
+    res.status(202).json({ data: updatedProject })
+  } catch (error) {
+    next(error);
+  }
 });
 
 // @desc    Delete a movie
 // @route   DELETE /:id
 // @access  Public
 router.delete('/:id', async (req, res, next) => {
-
+  const { id } = req.params;
+  try {
+    const deleted = await Project.findByIdAndDelete(id);
+    res.status(202).json({ data: deleted });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- seed/index.js
+++ seed/index.js
@@ -1,12 +1,31 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Project = require('../models/Project');
 
 // Add the model and array you want to seed
+const movierProjects = [
+  {
+    title: "Spiderman",
+    description: "Spiderman description blablabla"
+  },
+  {
+    title: "Titanic",
+    description: "Titanic description blablabla"
+  },
+  {
+    title: "The Simpsons",
+    description: "The Simpsons description blablabla"
+  },
+  {
+    title: "Minions",
+    description: "Minions description blablabla"
+  }
+]
 
 mongoose.connect(process.env.MONGO_URL)
   .then(x => console.log(`Connected to ${x.connection.name}`))
   .then(() => {
-    return
+    return Project.create(movierProjects)
   })
   .then(() => {
     console.log('Seed done 🌱');
@@ -16,3 +35,6 @@ mongoose.connect(process.env.MONGO_URL)
     console.log('Closing connection');
     mongoose.connection.close();
   })
+
+
+  
